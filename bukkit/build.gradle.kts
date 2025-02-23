f4bdf0d1f0163444b8807d1716f172026053b398
Siroshun09 <siro.shun8@gmail.com>
2025-02-23 19:31:29 +0900

    build(bukkit): update paper-api and placeholder-api

--- bukkit/build.gradle.kts
+++ bukkit/build.gradle.kts
@@ -15,8 +15,8 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT")
-    compileOnly("me.clip:placeholderapi:2.11.1")
+    compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
+    compileOnly("me.clip:placeholderapi:2.11.6")
     compileOnly("com.github.ucchyocean:LunaChat:v3.0.16") {
         exclude("org.bstats")
     }
